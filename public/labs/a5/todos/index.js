8591b79b2af24d0114cc1e254a5c6a3dbc2c3bed
Gowtham Potnuru <gouthamcall@gmail.com>
2022-10-13 12:23:27 -0400

    completed lab

---
+++ public/labs/a5/todos/index.js
@@ -0,0 +1,16 @@
+import TodoList from "./TodoList.js";
+const init = () => {
+    $('#wd-todo').append(`
+    <div class="container">
+        <h1>Todo example</h1>
+        ${TodoList()}
+    </div>
+    `);
+ }
+ 
+ $(init);
+ 
+ 
+ 
+ 
+ 
